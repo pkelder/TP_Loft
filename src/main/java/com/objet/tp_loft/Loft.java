d21507f5eda16c6c9dfdae0b1bd1a526b0c5cbc0
Lou <pauline.kelder@gmail.com>
2011-11-18 10:57:08 +0100

    j ai avancé la classe Loft

--- src/main/java/com/objet/tp_loft/Loft.java
+++ src/main/java/com/objet/tp_loft/Loft.java
@@ -4,43 +4,68 @@
  */
 package com.objet.tp_loft;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author tagazok
  */
 public class Loft {
     /*   Attributes   */
-
     protected Case[][] grille;
-    protected int largeurLoftX;
-    protected int longueurLoftY;
-    protected ArrayList<Nourriture> bouffeDispo;
-    protected int periodePrimeTime;
-    protected int tempsJusquaPrimeTime;
-
+	protected int largeurLoftX;
+	protected int longueurLoftY;
+	protected ArrayList<Nourriture> bouffeDispo;
+	protected int periodePrimeTime;
+	protected int tempsJusquaPrimeTime;
+	
+    
     /*   Constructors  */
-    public Loft() {
-        // getConf() : longueur, largeur, listeBouffe, périodePrimeTime
-    }
-
+	public Loft() {
+		// getConf() : longueur, largeur, listeBouffe, périodePrimeTime
+	}
+	
+	public loft (int largeur,int longueur){
+		this.largeurLoftX=largeur;
+		this.longueurLoftY=longueur;
+		for (int i=0;i<largeur;i++){
+			for (int j=0;j<longueur;j++){
+		this.grille[i][j]=new Case();
+			}
+			}
+	}
+	
+    
     /*   Methods   */
-    public void tour() {
-        // Déplacement + Action + PrimeTime
+	public void tour() {
+		// Déplacement + Action + PrimeTime
+	}
+    
+	public void ajoutBouffe(Nourriture bouffe, int posX, int posY) {
+		// Choix quantité + type + random position
+		// Vérifier case libre
+		//test si bouffe
+		Case currentCase=this.grille[posX][posY];
+		if (currentCase.bouffe != null)
+				{
+					//on teste si le type de nourriture est le m�me
+					if (bouffe.type=currentCase.bouffe.type){
+						//on rajoute de la nourriture
+						currentCase.bouffe.valeurEnergetique=currentCase.bouffe.valeurEnergetique+bouffe.valeurEnergetique;
+					}
+				}
+		//s'il n'y a pas de bouffe sur la case, on en met
+		currentCase.bouffe=bouffe;
+	}
+    
+	
+	public void ajoutNeuneu(Neuneu neuneu, int posX, int posY) {
+		Case currentCase=this.grille[posX][posY];
+		currentCase.neuneuSurCase.add(neuneu);
+		neuneu.positionX=posX;
+		neuneu.positionY=posY;
     }
-
-    public void ajoutBouffe() {
-        // Choix quantité + type + random position
-        // Vérifier case libre
-        // utiliser ajoutElement()
-    }
-
-    public void ajoutElement(Element element, int posX, int posY) {
-    }
-
-    public void primeTime() {
-        // Teste s'il y a primetime
-        // Ajoute nourriture + neuneus
-    }
+    
+	public void primeTime() {
+		// Teste s'il y a primetime
+		// Ajoute nourriture + neuneus
+	}
 }
