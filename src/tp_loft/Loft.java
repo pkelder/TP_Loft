3eab88de11b87a40b6959edd22fbade77ce3b49f
JunkieLand <deep.dark.night@gmail.com>
2011-11-22 10:19:19 +0100

    Teste de l interface. Elle marche très bien ! Correction de 2 - 3 trucs ailleurs

--- src/tp_loft/Loft.java
+++ src/tp_loft/Loft.java
@@ -20,28 +20,31 @@ public class Loft {
     protected int nbNeuneuTotal;
 
     public Loft() {
+        // Instanciation de la grille
+        this.grille = new Case[Loft.largeurLoftX][Loft.longueurLoftY];
+        // Instanciation des cases de la grille
         for (int i = 0; i < Loft.largeurLoftX; i++) {
             for (int j = 0; j < Loft.longueurLoftY; j++) {
-                this.grille[i][j] = new Case(i,j);
+                this.grille[i][j] = new Case(i, j);
             }
         }
     }
 
     public void tour() {
-        
+
         // Un balayage de la grille pour marcher()
         for (int i = 0; i < Loft.largeurLoftX; i++) {
             for (int j = 0; j < Loft.longueurLoftY; j++) {
                 Case currentCase = this.grille[i][j];
-                
-                if (currentCase.aNeuneu()){
-                    for (Neuneu neuneu : currentCase.neuneuSurCase){
-                        neuneu.marcher();                        
+
+                if (currentCase.aNeuneu()) {
+                    for (Neuneu neuneu : currentCase.neuneuSurCase) {
+                        neuneu.marcher();
                     }
                 }
             }
         }
-        
+
         // un autre balayage pour action(), 
         for (int i = 0; i < Loft.largeurLoftX; i++) {
             for (int j = 0; j < Loft.longueurLoftY; j++) {
@@ -49,68 +52,75 @@ public class Loft {
                 currentCase.action();
             }
         }
-        
+
         // un autre pour nettoyer les neuneus à zéro d'énergie (la bouffe est supprimée dans manger() )
         for (int i = 0; i < Loft.largeurLoftX; i++) {
             for (int j = 0; j < Loft.longueurLoftY; j++) {
                 Case currentCase = this.grille[i][j];
-                
-                if (currentCase.aNeuneu()){
-                    for (Neuneu neuneu : currentCase.neuneuSurCase){
-                        if (neuneu.getEnergie() == 0) currentCase.supprimerNeuneu(neuneu);
+
+                if (currentCase.aNeuneu()) {
+                    for (Neuneu neuneu : currentCase.neuneuSurCase) {
+                        if (neuneu.getEnergie() == 0) {
+                            currentCase.supprimerNeuneu(neuneu);
+                        }
                     }
                 }
-            
+
             }
         }
-        
+
         // Random pour le nombre de bouffe et de neuneu à rajouter
-        
-        int nbBouffe = (int) (Math.random()*5);
-        int nbNeuneu = (int) (Math.random()*2);
-            
-        for (int i = 0; i < nbBouffe; i++){
+
+        int nbBouffe = (int) (Math.random() * 5);
+        int nbNeuneu = (int) (Math.random() * 2);
+
+        for (int i = 0; i < nbBouffe; i++) {
             this.ajoutBouffe();
-        } 
-        
-        if (nbNeuneu == 1) this.ajoutNeuneu() ;
-        
-        
+        }
+
+        if (nbNeuneu == 1) {
+            this.ajoutNeuneu();
+        }
+
+
     }
 
     public void ajoutBouffe() {
         // random type 
-        int typeRandom = (int) (Math.random()*Loft.bouffeDispo.size());
+        int typeRandom = (int) (Math.random() * Loft.bouffeDispo.size());
         Nourriture Bouffe = Loft.bouffeDispo.get(typeRandom);
-        
+
         //random position + vérifier case libre
         boolean libre = false;
         int x = 0;
         int y = 0;
-        
-        while (!libre){
-            
-            x = (int) (Math.random()*Loft.largeurLoftX);
-            y = (int) (Math.random()*Loft.longueurLoftY);
-            
-            if (!this.grille[x][y].aNourriture()) libre = true ; 
-                    
+
+        while (!libre) {
+
+            x = (int) (Math.random() * Loft.largeurLoftX);
+            y = (int) (Math.random() * Loft.longueurLoftY);
+
+            if (!this.grille[x][y].aNourriture()) {
+                libre = true;
+            }
+
         }
-        
+
         // ajouter Bouffe dans la case sélectionnée
         this.grille[x][y].ajouterNourriture(Bouffe);
-        
+
     }
 
     public void ajoutNeuneu() {
         // Choix du type de neuneu au hasard + random position
         // Utiliser ajoutNeuneu() de la case
-        
     }
-    
+
     public int getNbNeuneuTotal() {
         return this.nbNeuneuTotal;
     }
-    
-   
+
+    public Case[][] getGrille() {
+        return this.grille;
+    }
 }
--- src/tp_loft/Main.java
+++ src/tp_loft/Main.java
@@ -4,7 +4,6 @@
  */
 package tp_loft;
 
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,73 +17,63 @@ import java.util.Properties;
  *
  * @author tagazok
  */
-
-
-
-
-
-
 public class Main {
 
     protected String pathFichierConf;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // Appel getConf() et initialisation()
-    	getConf("./src/config.properties");
-    	initialisation();
+        getConf("./src/config.properties");
+        initialisation();
         // Appel tour() qu	and le timer le dit, et tant tour() renvoie true
-    //	getConf("loft.largeur","Loft.largeurloftX");
-    	
-    	
+        //	getConf("loft.largeur","Loft.largeurloftX");
     }
 
-    
-    
-   	public static void getConf(String path) throws FileNotFoundException, IOException {
-    	       
+    public static void getConf(String path) throws FileNotFoundException, IOException {
+
         // Va chercher les propriétés dans le fichier de conf
         // Mets les valeurs static dans les classes correspondantes
-    	
-    	Properties props = new Properties();
-		props.load(new FileInputStream(path));
-		
-		//config du loft
-		Loft.largeurLoftX=Integer.parseInt(props.getProperty("loft.largeur"));
-		Loft.longueurLoftY=Integer.parseInt(props.getProperty("loft.longueur"));
-		Loft.periodeTour=Integer.parseInt(props.getProperty("loft.periodeTour"));
-		
-		//config de Neuneu
-		Neuneu.depenseMarcher=Integer.parseInt(props.getProperty("Neuneu.depenseMarcher"));
-		Neuneu.depenseSexe=Integer.parseInt(props.getProperty("Neuneu.depenseSexe"));
-		Neuneu.energieMax=Integer.parseInt(props.getProperty("Neuneu.energieMax"));
-		
-		//config de bouffeDispo dans loft
-		List<String> type=Arrays.asList(props.getProperty("Nourriture.liste").split(","));
-		List<String> valeurEnergetique=Arrays.asList(props.getProperty("Nourriture.valeurEnergetique").split(","));
-		
-		Loft.bouffeDispo=new ArrayList<Nourriture>();
-		
-		for (int i=0;i<type.size();i++){
-			String s=type.get(i);
-			int v=Integer.parseInt(valeurEnergetique.get(i));
-			Nourriture currentBouffe=new Nourriture(s,v);
-			System.out.println("s:"+s+" v: "+v);
-			Loft.bouffeDispo.add(currentBouffe);
-		}
-		
-	
+
+        Properties props = new Properties();
+        props.load(new FileInputStream(path));
+
+        //config du loft
+        Loft.largeurLoftX = Integer.parseInt(props.getProperty("loft.largeur"));
+        Loft.longueurLoftY = Integer.parseInt(props.getProperty("loft.longueur"));
+        Loft.periodeTour = Integer.parseInt(props.getProperty("loft.periodeTour"));
+
+        //config de Neuneu
+        Neuneu.depenseMarcher = Integer.parseInt(props.getProperty("Neuneu.depenseMarcher"));
+        Neuneu.depenseSexe = Integer.parseInt(props.getProperty("Neuneu.depenseSexe"));
+        Neuneu.energieMax = Integer.parseInt(props.getProperty("Neuneu.energieMax"));
+
+        //config de bouffeDispo dans loft
+        List<String> type = Arrays.asList(props.getProperty("Nourriture.liste").split(","));
+        List<String> valeurEnergetique = Arrays.asList(props.getProperty("Nourriture.valeurEnergetique").split(","));
+
+        Loft.bouffeDispo = new ArrayList<Nourriture>();
+
+        for (int i = 0; i < type.size(); i++) {
+            String s = type.get(i);
+            int v = Integer.parseInt(valeurEnergetique.get(i));
+            Nourriture currentBouffe = new Nourriture(s, v);
+            System.out.println("s:" + s + " v: " + v);
+            Loft.bouffeDispo.add(currentBouffe);
+        }
+
+
     }
 
     public static void initialisation() {
-    	
-    	
+
+
         // Instantie "new Case()" dans chaque case de "grille"
-    	for (int i=0;i<Loft.largeurLoftX;i++){
-    		for (int j=0;j<Loft.longueurLoftY;j++){
-    			Case currentCase=new Case();
-    		}
-    	}
-    	
+        for (int i = 0; i < Loft.largeurLoftX; i++) {
+            for (int j = 0; j < Loft.longueurLoftY; j++) {
+                Case currentCase = new Case();
+            }
+        }
+
         // Place la bouffe et les neuneu de départ. Critère :
         //		partie entière du 1/4 du nombre de cases
     }
