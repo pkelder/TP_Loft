074dfb3827e38e71ecb42636a83d9147970c9ef4
Ludivine Siau <ludivinesiau@gmail.com>
2011-11-23 01:59:35 +0100

    correction cannibale

--- src/tp_loft/Cannibale.java
+++ src/tp_loft/Cannibale.java
@@ -32,7 +32,7 @@ public class Cannibale extends Neuneuphile {
         
         Case position=loft.grille[this.getPosX()][this.getPosY()];
     	
-    	if (!position.aNeuneu()){
+    	if (position.fullNeuneu()){
         // S'il est tout seul : il bouffe
     		if (position.aNourriture()){
     			this.manger(position.getNourriture());
